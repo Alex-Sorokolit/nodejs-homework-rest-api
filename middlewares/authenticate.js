dcb80ed85cf2686426b70602a82dfd61c4b59ba0
Alex-Sorokolit <sorokolital@gmail.com>
2023-03-05 08:58:29 +0200

    ifNotToken

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -11,7 +11,7 @@ const authenticate = async (req, res, next) => {
   const [bearer, token] = authorization.split(" ");
 
   // перевіряємо чи Bearer
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401, "Not authorized"));
   }
 
